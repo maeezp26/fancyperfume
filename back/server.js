6b59cb40c727713a8169795d30c6aa1cf2d1b107
Maeez Pathan <mazu262002@gmail.com>
2026-03-20 15:52:54 +0530

    ready for vercel deploy

--- back/server.js
+++ back/server.js
@@ -30,7 +30,14 @@ const razorpay = new Razorpay({
 
 // Middlewares
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://your-frontend.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
